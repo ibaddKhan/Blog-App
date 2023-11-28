9fde93227cc5efa6d67291bf195f0b0bfd7f74a7
Ibad <136696616+ibaddKhan@users.noreply.github.com>
2023-11-29 00:21:14 +0500

    condition done

--- scripts/config.js
+++ scripts/config.js
@@ -1,15 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBRMu_-_yEejRHw9FE-TjblMxUikccWbvs",
-    authDomain: "blogging-app-ad12e.firebaseapp.com",
-    projectId: "blogging-app-ad12e",
-    storageBucket: "blogging-app-ad12e.appspot.com",
-    messagingSenderId: "10495524272",
-    appId: "1:10495524272:web:d6893b23fb91458097ea75"
+  apiKey: "AIzaSyDOag5XID0IPtXmBObpd1qQZq-vUspjdPQ",
+  authDomain: "bloggers--hub.firebaseapp.com",
+  projectId: "bloggers--hub",
+  storageBucket: "bloggers--hub.appspot.com",
+  messagingSenderId: "92005196041",
+  appId: "1:92005196041:web:c4023d5c33e17ae478f3d8",
 };
 
 export const app = initializeApp(firebaseConfig);
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,34 +1,29 @@
-import {  createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { auth} from "./config.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { auth } from "./config.js";
 
+const fname = document.querySelector("#f-name");
+const lname = document.querySelector("#l-name");
+const email = document.querySelector("#email");
+const pass = document.querySelector("#pass");
+const Rpass = document.querySelector("#r-pass");
+const form = document.querySelector("form");
+const button = document.querySelector("#btn");
 
-
-const fname = document.querySelector('#f-name')
-const lname = document.querySelector('#l-name')
-const email = document.querySelector('#email')
-const pass = document.querySelector('#pass')
-const Rpass = document.querySelector('#r-pass')
-const form = document.querySelector('form')
-const button = document.querySelector('#btn')
-
-
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    console.log(email.value);
-    console.log(pass.value);
-    createUserWithEmailAndPassword(auth, email.value, pass.value)
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (pass.value !== Rpass.value) {
+    alert("Passwords are not Same");
+    return;
+  }
+  createUserWithEmailAndPassword(auth, email.value, pass.value)
     .then((userCredential) => {
-        // Signed up 
-        const user = userCredential.user;
-        console.log(user);
-            // ...
-
-        })
-        .catch((error) => {
-
-            const errorMessage = error.message;
-            console.log(errorMessage);
-        });
-
-})
+      // Signed up
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+});
